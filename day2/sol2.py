2b81e57d314c59c870eedbd3ee9da0bda54465bc
Arseni <arseni_mars97@hotmail.com>
2023-12-02 09:34:42 +0100

    day2

---
+++ day2/sol2.py
@@ -0,0 +1,39 @@
+def read_all_lines(filename):
+    """Read all lines from a file and return them as a list."""
+    with open(filename, 'r') as file:
+        return file.readlines()
+
+s = 0
+d = {"red": 12, "green": 13, "blue": 14}
+for c, x in enumerate(read_all_lines("input"), 1):
+    x = x.strip()
+    y = x.split(": ")[1]
+    game = True
+    for l in y.split("; "):
+        for k in l.split(", "):
+            k = k.split(" ")
+            if d[k[1]] < int(k[0]):
+                print(x)
+                game = False
+                break
+    if game:
+        s += c
+
+print(s)
+
+
+s = 0
+for c, x in enumerate(read_all_lines("input"), 1):
+    d = {"red": 0, "green": 0, "blue": 0}
+    x = x.strip()
+    y = x.split(": ")[1]
+    for l in y.split("; "):
+        for k in l.split(", "):
+            k = k.split(" ")
+            if d[k[1]] < int(k[0]):
+                d[k[1]] = int(k[0])
+    product = 1
+    for value in d.values():
+        product *= value
+    s += product
+print(s)
